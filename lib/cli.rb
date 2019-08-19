c52fb296c0e33eb64647c359ea37009b2f212d60
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-08-19 14:27:15 -0400

    Fixed welcome method

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,17 +2,20 @@ def welcome
   puts "Welcome to Random Meetup Finder!"
   puts "Have you used this app before? (Y/N)"
   user_input = STDIN.gets.chomp.downcase
+
   if user_input == "n"
     puts "Let's make an account"
     puts "What is your name ?"
     new_username = STDIN.gets.chomp
-    user_id = User.create(name: new_username).id
+    user_id= User.create(name: new_username).id
+    main_menu(user_id)
   elsif user_input == "y"
     puts "What is your user id?"
-    returning_user_id = STDIN.gets.chomp
+    returning_user_id = STDIN.gets.chomp.to_i
     user_id = returning_user_id
+    main_menu(user_id)
   end
-  main_menu(user_id)
+
 end
 
 def main_menu(user_id)
@@ -23,5 +26,5 @@ def main_menu(user_id)
   puts "3 - See a group's upcoming meetings."
   puts "4 - Leave a group."
   puts "Enter the number of the menu item that you would like to access :"
-  user_input  = STDIN.gets.chomp.to_i 
+  user_input  = STDIN.gets.chomp.to_i
 end
