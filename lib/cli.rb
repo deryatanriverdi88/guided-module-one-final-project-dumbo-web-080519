8685888ea3a1571f9211062f23ac6a47dc8d29b6
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-08-19 14:12:40 -0400

    Starting making menu

---
+++ lib/cli.rb
@@ -0,0 +1,27 @@
+def welcome
+  puts "Welcome to Random Meetup Finder!"
+  puts "Have you used this app before? (Y/N)"
+  user_input = STDIN.gets.chomp.downcase
+  if user_input == "n"
+    puts "Let's make an account"
+    puts "What is your name ?"
+    new_username = STDIN.gets.chomp
+    user_id = User.create(name: new_username).id
+  elsif user_input == "y"
+    puts "What is your user id?"
+    returning_user_id = STDIN.gets.chomp
+    user_id = returning_user_id
+  end
+  main_menu(user_id)
+end
+
+def main_menu(user_id)
+  puts "Welcome #{User.find_name_by_id(user_id)}!"
+  puts "Main Menu :"
+  puts "1 - Find a random group."
+  puts "2 - See what groups you are in."
+  puts "3 - See a group's upcoming meetings."
+  puts "4 - Leave a group."
+  puts "Enter the number of the menu item that you would like to access :"
+  user_input  = STDIN.gets.chomp.to_i 
+end
